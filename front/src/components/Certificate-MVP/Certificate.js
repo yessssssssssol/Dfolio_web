d1625d67f5255ce8f45a91f4562b0fcfd4a31f8f
pys <whassup_man@naver.com>
2022-03-16 15:59:21 +0900

    Add certificate.js

--- front/src/components/Certificate-MVP/Certificate.js
+++ front/src/components/Certificate-MVP/Certificate.js
@@ -1,2 +1,27 @@
-import React, { useState, useContext } from "react";
-import { Container, Col, Row, Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import Certificate from "./CertificateCard";
+import CertificateEditForm from "./CertificateEditForm";
+
+function Certificate({ certificate, setCertificates, isEditable }) {
+  //useState로 isEditing 상태를 생성함.import React, { useState } from "react";
+  const [isEditing, setIsEditing] = useState(false);
+  return (
+    <>
+      {isEditing ? (
+        <CertificateEditForm
+          currentCertificate={Certificate}
+          setCertificates={setCertificates}
+          setIsEditing={setIsEditing}
+        />
+      ) : (
+        <CertificateCard
+        certificate={certificate}
+          isEditable={isEditable}
+          setIsEditing={setIsEditing}
+        />
+      )}
+    </>
+  );
+}
+
+export default Certificate;
