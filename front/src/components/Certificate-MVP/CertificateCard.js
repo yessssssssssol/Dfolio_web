5c00d0dcaa921a890023f3cbfef1cfa81625e121
pys <whassup_man@naver.com>
2022-03-16 15:50:04 +0900

    Add certificateForm

---
+++ front/src/components/Certificate-MVP/CertificateCard.js
@@ -0,0 +1,29 @@
+import { Card, Button, Row, Col } from "react-bootstrap";
+
+function CertificateCard({ certificate, isEditable, setIsEditing }) {
+  return (
+    <Card.Text>
+      <Row className="align-items-center">
+        <Col>
+          <span>{certificate.title}</span>
+          <br />
+          <span className="text-muted">{certificate.description}</span>
+        </Col>
+        {isEditable && (
+          <Col xs lg="1">
+            <Button
+              variant="outline-info"
+              size="sm"
+              onClick={() => setIsEditing((prev) => !prev)}
+              className="mr-3"
+            >
+              편집
+            </Button>
+          </Col>
+        )}
+      </Row>
+    </Card.Text>
+  );
+}
+
+export default CertificateCard;
