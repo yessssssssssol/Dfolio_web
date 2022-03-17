ed873a46fe46c408e1ccf9e7089e8e892c13d1aa
HyeJuSeon <hyejooseon@gmail.com>
2022-03-17 17:11:51 +0900

    Add awardRouter for update

--- back/src/routers/awardRouter.js
+++ back/src/routers/awardRouter.js
@@ -6,6 +6,7 @@ import { AwardService } from '../services/awardService';
 const awardRouter = Router();
 awardRouter.use(login_required);
 
+// Create Award
 awardRouter.post('/award/create', async (req, res, next) => {
   try {
     if (is.emptyObject(req.body)) {
@@ -18,6 +19,28 @@ awardRouter.post('/award/create', async (req, res, next) => {
       description,
     });
     res.status(201).json(newAward);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Update Award
+awardRouter.put('/awards/:id', async (req, res, next) => {
+  try {
+    const award_id = req.params.id;
+    const title = req.body.title ?? null;
+    const description = req.body.description ?? null;
+    const updateValue = { title, description };
+    const updatedAward = await AwardService.updateAward({
+      awardId,
+      updateValue,
+    });
+
+    // Error
+    if (updatedAward.errorMessage) {
+      throw new Error(updatedAward.errorMessage);
+    }
+    res.status(200).json(updatedAward);
   } catch (err) {
     next(err);
   }
