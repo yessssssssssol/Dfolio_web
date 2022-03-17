c464c7283807b9d9cdbe074722928df91a085cb2
HyeJuSeon <hyejooseon@gmail.com>
2022-03-17 17:22:36 +0900

    Add update to Award

--- back/src/db/models/Award.js
+++ back/src/db/models/Award.js
@@ -5,6 +5,19 @@ class Award {
     const createdNewAward = await AwardModel.create(awardData);
     return createdNewAward;
   }
+
+  static async update({ awardId, fieldToUpdate, value }) {
+    const filter = { id: awardId };
+    const update = { [fieldToUpdate]: value };
+    const option = { returnOriginal: false };
+    const updatedUser = await AwardModel.findOneAndUpdate(
+      filter,
+      update,
+      option,
+    );
+
+    return updatedUser;
+  }
 }
 
 export { Award };
