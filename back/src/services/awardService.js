9d06d50025891acbfad8fdd2bf9a86a6a892d19c
HyeJuSeon <hyejooseon@gmail.com>
2022-03-17 17:18:26 +0900

    Add updateAward to awardService

--- back/src/services/awardService.js
+++ back/src/services/awardService.js
@@ -2,12 +2,35 @@ import { Award } from '../db/models/Award';
 import { v4 as uuidv4 } from 'uuid';
 
 class AwardService {
+  // Create Award
   static async createAward({ userId, title, description }) {
     const awardId = uuidv4();
 
     const awardData = { id: awardId, userId, title, descripttion };
     const newAward = await Award.create({ awardData });
     return newAwawrd;
+  }
+
+  // Update Award
+  static async updateAward({ awardId, updateValue }) {
+    let award = await Award.findById({ awardId });
+    // Exception
+    if (!award) {
+      const errorMessage = '일치하는 awardId가 없습니다.';
+      return { errorMessage };
+    }
+
+    if (updateValue.title) {
+      const fieldToUpdate = 'title';
+      const value = updateValue.title;
+      award = await Award.update({ awardId, fieldToUpdate, value });
+    }
+    if (updateValue.description) {
+      const fieldToUpdate = 'description';
+      const value = updateValue.description;
+      award = await Award.update({ award_id, fieldToUpdate, value });
+    }
+    return award;
   }
 }
 
