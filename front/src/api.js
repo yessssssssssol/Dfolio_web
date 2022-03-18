3f07bfb3afbf707f16e7b9ee1efa4fd04383c763
jiiiooiiij <88502596+HANJISUN@users.noreply.github.com>
2022-03-18 22:21:47 +0900

    Add gitignore

--- front/src/api.js
+++ front/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const backendPortNumber = "5007";
+const backendPortNumber = "5003";
 
 const serverUrl =
   "http://" + window.location.hostname + ":" + backendPortNumber + "/";
