8070fd41f56b4ff3b5fb25526b8970a72d5ee6e7
Sang <sangeon_pak@naver.com>
2022-03-19 00:36:58 +0900

    firstCommit

---
+++ back/src/db/models/Education.js
@@ -0,0 +1,31 @@
+import { EducationModel } from '../schemas/education';
+
+
+class Education {
+  static async create({ newEducation }) {
+    const createdNewEducation = await EducationModel.create(newEducation);
+    return createdNewEducation;
+  }
+  static async findById({ id }) {
+    const education = await EducationModel.findOne({ id });
+    return education;
+  }
+  static async findAll({ userId }) {
+    const educations = await EducationModel.find({ userId });
+    return educations;
+  }
+  static async update({ id, fieldToUpdate, newValue }) {
+    const filter = { id };
+    const update = { [fieldToUpdate]: newValue };
+    const option = { returnOriginal: false };
+
+    const updatedEducation = await EducationModel.findOneAndUpdate(
+      filter,
+      update,
+      option,
+    );
+    return updatedEducation;
+  }
+}
+
+export { Education };
---
+++ front/src/components/Education_MVP/EducationAddForm.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { Button, Form, Col, Row } from "react-bootstrap";
+import * as Api from "../../api";
+
+function EducationAddForm({ portfolioOwnerId, setEducations, setIsAdding }) {
+  //school로 description 상태를 생성함.
+  const [school, setSchool] = useState("");
+  //major로 description 상태를 생성함.
+  const [major, setMajor] = useState("");
+  //position로 description 상태를 생성함.
+  const [position, setPosition] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    // portfolioOwnerId를 user_id 변수에 할당함.
+    const userId = portfolioOwnerId;
+
+    // "Education/create" 엔드포인트로 post요청함.
+    await Api.post("education/create", {
+      userId: portfolioOwnerId,
+      school,
+      major,
+      position,
+    });
+
+    // "Educationlist/유저id" 엔드포인트로 get요청함.
+    const res = await Api.get("educationlist", userId);
+    // Educations를 response의 data로 세팅함.
+    setEducations(res.data);
+    // Education를 추가하는 과정이 끝났으므로, isAdding을 false로 세팅함.
+    setIsAdding(false);
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formBasicTitle">
+        <Form.Control
+          type="text"
+          placeholder="학교 이름"
+          value={school}
+          onChange={(e) => setSchool(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group controlId="formBasicTitle" className="mt-3">
+        <Form.Control
+          type="text"
+          placeholder="전공"
+          value={major}
+          onChange={(e) => setMajor(e.target.value)}
+        />
+      </Form.Group>
+
+      <div key={"inline-radio"} className="mb-3">
+        <Form.Check
+          inline
+          label="재학중"
+          name="position"
+          type={"radio"}
+          id={`inline-radio-1`}
+        />
+        <Form.Check
+          inline
+          label="학사졸업"
+          name="position"
+          type={"radio"}
+          id={`inline-radio-2`}
+        />
+        <Form.Check
+          inline
+          label="석사졸업"
+          name="position"
+          type={"radio"}
+          id={`inline-radio-3`}
+        />
+        <Form.Check
+          inline
+          label="박사졸업"
+          name="position"
+          type={"radio"}
+          id={`inline-radio-4`}
+        />
+      </div>
+
+      {/* <Form.Group controlId="formBasicTitle">
+        <Form.Control
+          type="text"
+          placeholder="라디오 버튼"
+          value={position}
+          onChange={(e) => setPosition(e.target.value)}
+        />
+      </Form.Group> */}
+
+      <Form.Group as={Row} className="mt-3 text-center">
+        <Col sm={{ span: 20 }}>
+          <Button variant="primary" type="submit" className="me-3">
+            확인
+          </Button>
+          <Button variant="secondary" onClick={() => setIsAdding(false)}>
+            취소
+          </Button>
+        </Col>
+      </Form.Group>
+    </Form>
+  );
+}
+
+export default EducationAddForm;
