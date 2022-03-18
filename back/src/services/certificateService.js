d7a1a4c1c55a27732e866382aa42b068099bea9e
Sue <mooon2205@gmail.com>
2022-03-18 16:23:51 +0900

    Add delete

--- back/src/services/certificateService.js
+++ back/src/services/certificateService.js
@@ -64,6 +64,19 @@ class certificateAuthService {
 
     return certificate;
   }
+
+  static async deleteCertificate({ id }) {
+    const isDataDeleted = await Certificate.deleteById({ id });
+
+    // db에서 찾지 못한 경우, 에러 메시지 반환
+    if (!isDataDeleted) {
+      const errorMessage =
+        '해당 id를 가진 수상 데이터는 없습니다. 다시 한 번 확인해 주세요.';
+      return { errorMessage };
+    }
+
+    return { status: 'ok' };
+  }
 }
 
 export { certificateAuthService };
