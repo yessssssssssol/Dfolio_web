86a9d521c455f3da35c2346daf58516bd9b92a0d
HyeJuSeon <hyejooseon@gmail.com>
2022-03-19 13:21:40 +0900

    Add deleteAward to AwardService

--- back/src/services/awardService.js
+++ back/src/services/awardService.js
@@ -49,6 +49,19 @@ class AwardService {
     const awardList = await Award.findByUserId({ userId });
     return awardList;
   }
+
+  // Delete Award
+  static async deleteAward({ id }) {
+    const isDataDeleted = await Award.deleteById({ id });
+
+    // db에서 찾지 못한 경우, 에러 메시지 반환
+    if (!isDataDeleted) {
+      const errorMessage =
+        '해당 id를 가진 수상 데이터는 없습니다. 다시 한 번 확인해 주세요.';
+      return { errorMessage };
+    }
+    return { status: 'ok' };
+  }
 }
 
 export { AwardService };
