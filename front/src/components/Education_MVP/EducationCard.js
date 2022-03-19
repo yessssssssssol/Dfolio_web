4e08d536e5e146ced3b7c11e4b9e1fab2a3a6bc6
[4기-2반] 박상언 <sangeon_pak@naver.com>
2022-03-19 17:14:36 +0000

    Update EducationCard.js

--- front/src/components/Education_MVP/EducationCard.js
+++ front/src/components/Education_MVP/EducationCard.js
@@ -1,7 +1,28 @@
 import React from "react";
 import { Card, Button, Row, Col } from "react-bootstrap";
+import * as Api from "../../api";
+import moment from "moment";
 
-const EducationCard = ({ education, isEditable, setIsEditing }) => {
+function EducationCard({ education, isEditable, setIsEditing, setEducations }) {
+  const handleDelete = async (e) => {
+    //  페이지가 리프레쉬 되는 고유의 브라우저 동작을 preventDefault()로 막아줌
+    e.preventDefault();
+    // 부모 엘리먼트에게 이벤트 전달을 중단해야 할 때 쓰이는 함수
+    e.stopPropagation();
+
+    const userId = education.userId;
+
+    // certificate.id로 조회하여 데이터 삭제
+    await Api.delete(`educations/${education.id}`);
+    //console.log(education.id);
+    // "certificatelist/:userId" 엔드포인트로 GET 요청함.
+    const res = await Api.get("educationlist", userId);
+    // certificates를 response의 data로 세팅함.
+    setEducations(res.data);
+    // 편집 과정이 끝났으므로, isEditing을 false로 세팅함.
+    setIsEditing(false);
+  };
+
   return (
     <Card.Body>
       <Row className="align-items-center">
@@ -11,6 +32,11 @@ const EducationCard = ({ education, isEditable, setIsEditing }) => {
           <span>{education.major}</span>
           <br />
           <span>{education.position}</span>
+          <span className="text-muted">
+            {`${moment(education.fromDate).format("YYYY-MM-DD")} ~ ${moment(
+              education.toDate
+            ).format("YYYY-MM-DD")}`}
+          </span>
         </Col>
         {isEditable && (
           <Col xs lg="1">
@@ -20,7 +46,16 @@ const EducationCard = ({ education, isEditable, setIsEditing }) => {
               onClick={() => setIsEditing((prev) => !prev)}
               className="mr-3"
             >
-              편집
+              Edit
+            </Button>
+
+            <Button
+              variant="outline-danger"
+              size="sm"
+              onClick={handleDelete}
+              className="mr-1"
+            >
+              Delete
             </Button>
           </Col>
         )}
