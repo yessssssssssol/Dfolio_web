8827a0630704bfa09a5104ac7dc79b6369fee14d
jiiiooiiij <88502596+HANJISUN@users.noreply.github.com>
2022-03-25 14:37:50 +0900

    Remove filter View button

--- front/src/components/user/Network.js
+++ front/src/components/user/Network.js
@@ -13,7 +13,6 @@ function Network() {
   const sort = [
     { name: "Recently", value: "updatedAt" },
     { name: "Like", value: "likeCount" },
-    { name: "View", value: "viweCount" },
   ];
   useEffect(() => {
     // 만약 전역 상태의 user가 null이라면, 로그인 페이지로 이동함.
